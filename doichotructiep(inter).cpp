be62ad1e51790b1937fe8360797c81efcc15c189
01PhiHung01 <125170335+01PhiHung01@users.noreply.github.com>
2023-04-11 22:42:48 +0700

    Create doichotructiep(inter).cpp

---
+++ doichotructiep(inter).cpp
@@ -0,0 +1,22 @@
+//doi cho truc tiep 
+#include <iostream>
+using namespace std;
+
+void Interchange_Sort(int a[],int n) {
+    int i,j;
+    for (i=0;i<n-1;i++) {
+        for (j=i+1;j<n;j++) {
+            if (a[i]>a[j]) {
+                swap(a[i],a[j]);
+            }
+        }
+    }
+}
+int main() {
+    int a[6] = {41,23,4,14,56,1};
+    Interchange_Sort(a, 6);
+    cout<<"Mang sau kh sap"<<endl;
+    for (int i=0;i<6;i++) {
+        cout<<a[i]<<" ";
+    }
+}
